e9dbe765f53e15d1db6b6e4678cf35ab94bbebf3
alikerock <alikerock@gmail.com>
2025-08-11 17:35:40 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
 
-    domains: ['kfowfrynubrxhzgpvjuy.supabase.co']
-  },
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/portfolio-nextjs_supabase' : '',
+  assetPrefix: isProd ? '/portfolio-nextjs_supabase/' : '',
+  images: { unoptimized: true },   // Pages에서 권장
 };
 
 export default nextConfig;

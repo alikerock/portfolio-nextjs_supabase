e9766c925b85a95a866ccb4e0c5e4c2ce7cb0bfa
alikerock <alikerock@gmail.com>
2025-08-11 17:39:57 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio-nextjs_supabase' : '',
-  assetPrefix: isProd ? '/portfolio-nextjs_supabase/' : '',
+  basePath: '/portfolio-nextjs_supabase',
+  assetPrefix: '/portfolio-nextjs_supabase/',
   images: { unoptimized: true },   // Pages에서 권장
 };
+
 
 export default nextConfig;
